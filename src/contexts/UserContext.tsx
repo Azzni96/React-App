01ea03895088fdd0e60452cfe6b9a2efde304e2d
Azzni96 <nihadazzam96@gmail.com>
2025-02-04 22:17:02 +0200

    Refactor UserContext to improve login handling; add TODOs for API integration and state management

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -18,19 +18,22 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
     // login, logout and autologin functions are here instead of components
     const handleLogin = async (credentials: Credentials) => {
       try {
+        // TODO: post login credentials to API
         const loginResult = await postLogin(credentials);
         console.log('doLogin result', loginResult);
+        // TODO: set token to local storage
         if (loginResult) {
-          localStorage.setItem('token', loginResult.token); // Ensure this matches the key used in Logout
-          const userResponse: UserResponse = await getUserByToken(loginResult.token);
-          setUser(userResponse.user);
-          navigate('/');
+          localStorage.setItem('token', loginResult.token);
         }
-      } catch (error) {
-        console.error((error as Error).message);
-        // Display error to user here(?)
+        // TODO: set user to state
+        setUser(loginResult.user);
+        // TODO: navigate to home
+        navigate('/');
+      } catch (e) {
+        console.log((e as Error).message);
       }
     };
+
 
 
 
@@ -45,28 +48,26 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
         }
     };
 
-    // handleAutoLogin is used when the app is loaded to check if there is a valid token in local storage
     const handleAutoLogin = async () => {
-        try {
-            const token = localStorage.getItem('token');
-            // TODO: if token exists, get user data from API
-            if (token) {
-                const userResponse = await getUserByToken(token);
-
-                if (!userResponse.user) {
-                  throw new Error(userResponse.message);
-                }
-                setUser(userResponse.user);
-                const origin = location.state.form.pathname || '/';
-                navigate(origin);
-            }
-
-            // TODO: set user to state
-            // TODO: navigate to home
-        } catch (e) {
-            console.log((e as Error).message);
+      try {
+        // TODO: get token from local storage
+        const token = localStorage.getItem('token');
+        // TODO: if token exists, get user data from API
+        if (!token) {
+          return;
         }
+        const userResponse: UserResponse = await getUserByToken(token);
+        // TODO: set user to state
+        setUser(userResponse.user);
+        // when page is refreshed, the user is redirected to origin (see ProtectedRoute.tsx)
+        const origin = location.state.from.pathname || '/';
+        navigate(origin);
+      } catch (e) {
+        // alert('Token not valid');
+        console.log((e as Error).message);
+      }
     };
+
     useEffect(() => {
         handleAutoLogin();
     }, []);
